58d245017ba41840bbf1910a1d2c17fb679e16d7
Markus Waltre <markus.waltre@gmail.com>
2014-04-28 19:32:22 +0800

    Trying examples with objects instead

--- js/keywords.js
+++ js/keywords.js
@@ -1,3 +1,40 @@
+// trying to create an object instead
+
+function keywords () {
+    this.keywords = new Array();
+    this.setKeywords = function() {
+        toTitleCaseArray();
+
+		key_arr_gl.length = 0;
+		for(var i=0; i<keywords.length; i++) {
+			key_arr_gl.push(keywords[i]);
+		}
+		drawKeywords(key_arr_gl);
+    };
+    this.toTitleCaseArray = function() {
+    	for(var i = 0; i<keywords.length; i++) {
+			keywords[i] = toTitleCase(array[i]);
+		}
+    };
+    this.toTitleCase = function() {
+    	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    };
+}
+
+// testing with object
+
+function apple () {
+	this.fruit = new Array();
+	this.setFruits = function(arr) {
+		fruit = arr;
+	};
+	this.printFruits = function() {
+		console.log(fruit);
+	};
+}
+
+// old version below
+
 Array.prototype.unset = function(value) {
     if(this.indexOf(value) != -1) { // Make sure the value exists
         this.splice(this.indexOf(value), 1);
@@ -21,12 +58,12 @@ function getKeywords() {
 function drawKeywords(keywords) {
 	var t_str = '';
 	for(var i=0; i<keywords.length; i++) {
-		t_str +=  "<span class='radius label'>" + keywords[i] + "</span> ";
+		t_str +=  "<span class='keyword'>" + keywords[i] + "</span> ";
 	}
 	$("#keywords").html(t_str);
 	
-	$('.label').off('click.label');
-		$(".label").on('click.label', function (e) {
+	$('.keyword').off('click.keyword');
+		$(".keyword").on('click.keyword', function (e) {
 	    key_arr_gl.unset($(this).html());
 	    drawKeywords(key_arr_gl);
 	});
@@ -52,6 +89,8 @@ function toTitleCaseArray(array){
 
 var key_arr_gl = new Array();
 
+
+// bugtesting
 var t = ["test1", "test2", "test3", "test4"];
 setKeywords(t);
 
@@ -61,3 +100,8 @@ while(true) {
 	count++;
 	if(count > 1) break;
 }
+
+var p = new keywords();
+var a = new apple();
+a.setFruits(["apple", "pear"]);
+a.printFruits();
