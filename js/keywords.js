a57d2127ac424c6802c96f5ecb5538386f46e7c1
Markus Waltre <markus.waltre@gmail.com>
2014-04-28 19:07:04 +0800

    Basic working example

--- js/keywords.js
+++ js/keywords.js
@@ -1,7 +1,6 @@
 Array.prototype.unset = function(value) {
     if(this.indexOf(value) != -1) { // Make sure the value exists
         this.splice(this.indexOf(value), 1);
-		saveKeywords();
     }   
 }
 
@@ -37,29 +36,28 @@ function addKeyword(new_tag) {
 	new_tag = toTitleCase(new_tag);
 	key_arr_gl.push(new_tag);
 	drawKeywords(key_arr_gl);
-	saveKeywords();
 }
 
-$("#keywordform").on('submit', function(e){
-	e.preventDefault();
-	var new_tag = $("#keywordinput").val();
-	$("#keywordinput").val("");
-	addKeyword(new_tag);
-})
+function toTitleCase(str){
+	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+function toTitleCaseArray(array){
+	for(var i = 0; i<array.length; i++) {
+		array[i] = toTitleCase(array[i]);
+	}
+
+	return array;
+}
 
 var key_arr_gl = new Array();
 
+var t = ["test1", "test2", "test3", "test4"];
+setKeywords(t);
 
-//// for google trends
-function setKeywordsGoogleTrends(keywords) {
-	var t_str = '';
-	for(var i=0; i<keywords.length; i++) {
-		t_str +=  "<span class='radius label'>" + keywords[i] + "</span> ";
-	}
-	$("#keywordsGoogleTrends").html(t_str);
-}
-
-function setKeywordsGoogleTrendsString(keywords_string) {
-	keywords = keywords_string.split(',');
-	setKeywordsGoogleTrends(keywords);
+var count = 0;
+while(true) {
+	addKeyword("hfa");
+	count++;
+	if(count > 1) break;
 }
