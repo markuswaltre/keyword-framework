5a4bd532775323a23e71e82b1ae8cfded3880547
Markus Waltre <markus.waltre@gmail.com>
2014-05-02 21:46:19 +0800

    Base framework complete
    Add, delete, get, show done

--- js/keywords.js
+++ js/keywords.js
@@ -1,107 +1,83 @@
 // trying to create an object instead
 
 function keywords () {
+	// to be able to use self-reference
+	// inside jquery $(this)
+	var self = this;
     this.keywords = new Array();
-    this.setKeywords = function() {
-        toTitleCaseArray();
+    this.titleCase = true;
+    this.spanName = 'keyword';
 
-		key_arr_gl.length = 0;
-		for(var i=0; i<keywords.length; i++) {
-			key_arr_gl.push(keywords[i]);
+    this.setTitleCase = function(bool) {
+    	// check if true or false here..
+    	this.titleCase = bool;
+    }
+    this.deleteKeyword = function(value) {
+    	//this.keywords.unset(val);
+    	console.log(value);
+    	if(this.keywords.indexOf(value) != -1) { // Make sure the value exists
+	        this.keywords.splice(this.keywords.indexOf(value), 1);
+	    } 
+    };
+    this.drawKeywords = function() {
+    	var t_str = '';
+		for(var i=0; i<this.keywords.length; i++) {
+			t_str +=  "<span class='keyword'>" + this.keywords[i] + "</span> ";
 		}
-		drawKeywords(key_arr_gl);
-    };
-    this.toTitleCaseArray = function() {
-    	for(var i = 0; i<keywords.length; i++) {
-			keywords[i] = toTitleCase(array[i]);
-		}
-    };
-    this.toTitleCase = function() {
-    	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-    };
-}
-
-// testing with object
-
-function apple () {
-	this.fruit = new Array();
-	this.setFruits = function(arr) {
-		fruit = arr;
-	};
-	this.printFruits = function() {
-		console.log(fruit);
-	};
-}
-
-// old version below
-
-Array.prototype.unset = function(value) {
-    if(this.indexOf(value) != -1) { // Make sure the value exists
-        this.splice(this.indexOf(value), 1);
-    }   
-}
-
-function setKeywords(keywords) {		
-	keywords = toTitleCaseArray(keywords);
-
-	key_arr_gl.length = 0;
-	for(var i=0; i<keywords.length; i++) {
-		key_arr_gl.push(keywords[i]);
-	}
-	drawKeywords(key_arr_gl);
-}
-
-function getKeywords() {
-	return key_arr_gl;
-}
-
-function drawKeywords(keywords) {
-	var t_str = '';
-	for(var i=0; i<keywords.length; i++) {
-		t_str +=  "<span class='keyword'>" + keywords[i] + "</span> ";
-	}
-	$("#keywords").html(t_str);
-	
-	$('.keyword').off('click.keyword');
+		$("#keywords").html(t_str);
+		
+		$('.keyword').off('click.keyword');
 		$(".keyword").on('click.keyword', function (e) {
-	    key_arr_gl.unset($(this).html());
-	    drawKeywords(key_arr_gl);
-	});
-}
+				self.deleteKeyword($(this).html());
+			    self.drawKeywords();
+		});
+    };
+    this.setKeywords = function(keywords_input) {
+    	if(this.titleCase) {
+    		keywords_input = this.toTitleCaseArray(keywords_input);
+    	}
+        // does this really clear the array?
+		this.keywords.length = 0;
+		for(var i=0; i<keywords_input.length; i++) {
+			this.keywords.push(keywords_input[i]);
+		}
+		this.drawKeywords();
+    };
+    this.getKeywords = function() {
+    	return this.keywords;
+    };
+    this.addKeyword = function(input) {
+    	input = $.trim(input); // delete whites
+		if(this.titleCase) {
+    		input = this.toTitleCase(input);
+    	}
+    	if(input.length > 0) { // simple check for length
+			this.keywords.push(input);
+			this.drawKeywords();
+		}
+    };
+    this.toTitleCaseArray = function(arr) {
+    	for(var i = 0; i<arr.length; i++) {
+			arr[i] = this.toTitleCase(arr[i]);
+		}
 
-function addKeyword(new_tag) {
-	new_tag = toTitleCase(new_tag);
-	key_arr_gl.push(new_tag);
-	drawKeywords(key_arr_gl);
-}
+		return arr;
+    };
+    this.toTitleCase = function(keyword) {
+    	return keyword.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    };
+};
 
-function toTitleCase(str){
-	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-}
-
-function toTitleCaseArray(array){
-	for(var i = 0; i<array.length; i++) {
-		array[i] = toTitleCase(array[i]);
-	}
-
-	return array;
-}
-
-var key_arr_gl = new Array();
-
-
-// bugtesting
-var t = ["test1", "test2", "test3", "test4"];
-setKeywords(t);
-
-var count = 0;
-while(true) {
-	addKeyword("hfa");
-	count++;
-	if(count > 1) break;
-}
+// testing
 
 var p = new keywords();
-var a = new apple();
-a.setFruits(["apple", "pear"]);
-a.printFruits();
+p.setTitleCase(true);
+p.setKeywords(["aaSa", "aa b", "AAH bbb AA", "hello", "you"]);
+
+// debugging
+
+$("#test").submit(function (event) {
+	event.preventDefault();
+	p.addKeyword($("#text").val());
+	$("#text").val("");
+});
